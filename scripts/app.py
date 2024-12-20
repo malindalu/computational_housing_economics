2f35b24dc1eae7b60b5d59b56b533295e3f442a7
Malinda Lu <malindalu@Malindas-MacBook-Pro-2.local>
2024-12-20 15:04:11 -0500

    fixed equality line graphs and added explanations

--- scripts/app.py
+++ scripts/app.py
@@ -27,11 +27,8 @@ with col2:
      # down_payment_percent = st.number_input("Enter your down payment as a percentage of total price (e.g. 50 for 50%):", value=50.00, format="%.2f")/100
      afr_fraction = st.number_input("Enter the fraction of AFR used as the reduced interest rate (Currently, the College sets it at 1/2 AFR)", value = WELLESLEY_AFR_FRACTION)
 df_bar = bar_graph.calculate_monthly_payments(bar_graph.filter_df(df), max_second_loan)
-# st.pyplot(bar_graph.generate_plot(df_bar, town))
 
 df_equality_line = bar_graph.calculate_monthly_payments(equality_line_graph.filter_df(df))
-# st.subheader('Equality Line Comparison For With and Without Program')
-# st.pyplot(equality_line_graph.plot_equality_graph(df_equality_line, town))
 
 st.subheader("Below are the results:")
 
@@ -39,12 +36,10 @@ col1,col2 = st.columns(2)
 with col1:
      st.header(town1)
      st.pyplot(bar_graph.generate_plot(df_bar, town1))
-     # st.pyplot(equality_line_graph.plot_equality_graph(df_equality_line, town1))
 
 with col2:
      st.header(town2)
      st.pyplot(bar_graph.generate_plot(df_bar, town2))
-     # st.pyplot(equality_line_graph.plot_equality_graph(df_equality_line, town2))
 
 st.text("Monthly mortgage payments with and without the program, from 2016 to 2024. We assume that home value = ZHVI (Zillow House Value Index), which measures the typical home value in the given town.")
 
@@ -54,3 +49,10 @@ with col1:
 
 with col2:
      st.pyplot(equality_line_graph.plot_equality_graph(df_equality_line, town2))
+
+st.text("Monthly mortgage payment with and without the program, compared with 1/3 of monthly average median income (AMI) for 2016 and 2024.")
+st.text("Note: each point represents the town in a specific year from 2016-2024. Greener points are closer to 2016 and bluer points are closer to 2024. ")
+st.subheader("For interpretation of the graphs:")
+st.text("- If a point is inside the AMI box, then the town can be considered affordable both with and without the program.")
+st.text("- If a point is above the AMI box, then the town can be considered unafforidable without the program but affordable with the program.")
+st.text("- If a point is outside the AMI box towards the top right, then the town can be considered unafforidable both with and without the program.")
--- scripts/equality_line_graph.py
+++ scripts/equality_line_graph.py
@@ -15,13 +15,19 @@ def plot_equality_graph(df, town):
     for year, color in zip(range(2016, 2025), ["green", "darkgreen", "forestgreen", "mediumseagreen", "mediumturquoise", 
                                                "darkcyan", "steelblue", "dodgerblue", "blue"]):
         yearly_data = df[df['year'] == year]
-        ax.scatter(yearly_data[f"{town}_CL"], yearly_data[f"{town}_MRRP"], color=color, label=str(year))
+        ax.scatter(yearly_data[f"{town}_MRRP"],yearly_data[f"{town}_CL"], color=color)
 
     # Plot reference lines
-    ax.plot([0, 10000], [0, 10000], color="gray", linestyle="--", label="y = x line")
-    ax.axhline(y=df[df['year'] == 2024]['AMI'].mean() / 36, color="red", linestyle="--", label="1/3 2024 Monthly AMI")
-    ax.axhline(y=df[df['year'] == 2016]['AMI'].mean() / 36, color="orange", linestyle="--", label="1/3 2016 Monthly AMI")
-    
+    # 45 degree line
+    ax.plot([0, 10000], [0, 10000], color="gray", linestyle="--")
+
+    ami_2016 = df[df['year'] == 2016]['AMI'].mean() / 36
+    ax.plot([0, ami_2016], [ami_2016, ami_2016],color="orange", linestyle="--", label="1/3 2016 Monthly AMI")
+    ax.plot([ami_2016, ami_2016],[0, ami_2016],color="orange", linestyle="--")
+    ami_2024  = df[df['year'] == 2024]['AMI'].mean() / 36
+    ax.plot([0, ami_2024], [ami_2024, ami_2024],color="red", linestyle="--", label="1/3 2024 Monthly AMI")
+    ax.plot([ami_2024, ami_2024],[0, ami_2024],color="red", linestyle="--")
+
     # Labels, title, and legend
     ax.set_xlabel(f"{town} with Program")
     ax.set_ylabel(f"{town} without Program")
